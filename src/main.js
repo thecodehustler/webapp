2f4568e17acf0dc3477eedc7eecaaf96c70bb4d8
HZ thecodehustler@gmail.com <thecodehustler@gmail.com>
2020-05-17 21:38:08 +0800

    5.17

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ let root = new Vue({
   vuetify,
   i18n: VI18NOptions,
   render: h => h(App), // 等效于 createElement: createElement(App),
-  beforeMount: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 });
 
 // if ('serviceWorker' in navigator) {
--- vue.config.js
+++ vue.config.js
@@ -62,82 +62,7 @@ module.exports = {
         })
       ]
     },
-
-    // productionGzip: true,
-    // productionGzipExtensions: ['js', 'css'],
-
-    
   },
-
-  // chainWebpack: () => {
-  //   if (process.env.NODE_ENV !== 'production') return;
-  //   return {
-  //     plugins: [
-  //       new CompressionPlugin({
-  //         filename: "[path].gz[query]",
-  //         test: /\.js$|\.html$|\.css$/,    //压缩 js 与 css
-  //         algorithm: 'gzip',
-  //         threshold: 10240,
-  //         minRatio: 0.8
-  //       }),
-  //       new PrerenderSPAPlugin({
-  //         // 生成文件的路径，也可以与webpakc打包的一致。
-  //         // 下面这句话非常重要！！！
-  //         // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
-  //         staticDir: path.join(__dirname, 'dist'),
-  //         // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
-  //         routes: ['/'],
-  //         // 这个很重要，如果没有配置这段，也不会进行预编译
-  //         renderer: new Renderer({
-  //           inject: {
-  //             foo: 'bar'
-  //           },
-  //           headless: false,
-  //           // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
-  //           renderAfterDocumentEvent: 'x-render-event'
-  //         })
-  //       })
-  //     ],
-  //   };
-  // },
-
-  // chainWebpack: config => {
-  //   config.when(process.env.NODE_ENV === 'production', config => {
-  //     config.entry('app').clear().add('./src/main-prod.js');
-
-  //     let externals = {
-  //       vue: "Vue",
-  //       // axios: 'axios',
-  //       vuetify: 'vuetify',
-  //       'vue-router': 'VueRouter',
-  //       'vuex': 'Vuex',
-  //       'lodash': 'LoDash',
-  //       'vue-cesium': 'VueCesium'
-  //     };
-  //     config.externals(externals);
-  //     const cdn = {
-  //       css: [
-  //       ],
-  //       js: [
-  //         'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js', //Vue
-  //         'https://cdn.jsdelivr.net/npm/vue-router@3.1.6/dist/vue-router.min.js', // Router
-  //         'https://cdn.jsdelivr.net/npm/vuetify@2.2.26/dist/vuetify.min.js', //Vuetify
-  //         'https://cdn.jsdelivr.net/npm/vue-cesium@2.1.0/lib/index.umd.js' // VueCesium
-  //         'https://cdn.jsdelivr.net/npm/vuex@3.3.0/dist/vuex.min.js', // vuex
-  //         // 'https://cdn.jsdelivr.net/npm/axios@0.19.2/index.min.js', //axios
-  //         'https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js', // LoDash
-  //       ]
-  //     };
-  //     config.plugin('html')
-  //       .tap(args => {
-  //         args[0].cdn = cdn
-  //         return args
-  //       });
-  //   });
-  //   config.when(process.env.NODE_ENV === 'development', config => {
-  //   });
-
-  // },
 
   lintOnSave: false,
 
@@ -148,8 +73,35 @@ module.exports = {
         '/'
       ],
       useRenderEvent: true,
-      headless: true,
+      headless: false,
       onlyProduction: true
+    },
+    externals: {
+      common: [
+        {
+          id: 'vue',
+          assets: 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
+          global: 'Vue'
+        },
+        {
+          id: 'vuetify',
+          assets: [
+            'https://cdn.jsdelivr.net/npm/vuetify@2.2.28/dist/vuetify.min.js',
+            'https://cdn.jsdelivr.net/npm/vuetify@2.2.28/dist/vuetify.min.css'
+          ],
+        },
+        {
+          id: 'axios',
+          assets: 'https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js',
+          global: 'axios'
+        },
+        {
+          id: 'vuex',
+          assets: 'https://cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js'
+        }
+        
+      ]
     }
-  }
+  },
+  
 }
